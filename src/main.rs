ac6419e9d7e28e1658385159f0ae037ee0f8c511
John Ladan <john@ladan.ca>
2023-05-13 12:21:26 -0400

    move the request-from-bufreader logic to request module

--- src/main.rs
+++ src/main.rs
@@ -13,34 +13,21 @@ fn main() -> std::io::Result<()> {
 
     for stream in listener.incoming().take(3) {
         let mut stream = stream.unwrap();
+        // Read the stream as a request
         let mut buf_reader = BufReader::new(&stream);
-        // let mut buf: Vec<u8> = Vec::new();
-        let mut buf: String = String::new();
-        // stream.read_to_end(&mut buf)?;
-        let request = loop {
-            buf_reader.read_line(&mut buf)?;
-            let r = request::parse_request(&buf.as_bytes());
-            match r {
-                Err(ReqError::Incomplete) => continue,
-                _ => break r,
-            }
-        };
-        if let Ok(req) = request {
-            process_request(req);
+        let req = request::from_bufread(&mut buf_reader);
+        println!("{:#?}", req);
+        if let Ok(_req) = req {
+            let encoded = response_to_bytes(respond_hello_world()).unwrap();
+            println!("{}", encoded);
+            stream.write_all(&encoded.as_bytes())?;
+        } else if let Err(ReqError::IO(e)) = req {
+                return Err(e);
         }
-        let encoded = response_to_bytes(respond_hello_world()).unwrap();
-        println!("{}", encoded);
-        stream.write_all(&encoded.as_bytes())?;
+
     }
 
     Ok(())
-}
-
-fn process_request(request: http::Request<String>) {
-    if request.method() == http::Method::GET {
-        let resp = respond_hello_world();
-        println!("{}", response_to_bytes(resp).unwrap());
-    }
 }
 
 fn response_to_bytes(resp: http::Response<String>) -> Result<String, ResError> {
--- src/request.rs
+++ src/request.rs
@@ -1,9 +1,26 @@
 //! Submodule for parsing and managing HTTP requests
 //!
 
-use std::string;
+use std::{string, io::BufRead};
 
 const MAX_HEADERS: usize = 100;
+
+pub fn from_bufread(buf_reader: &mut impl BufRead) 
+        -> Result<http::Request<String>, ReqError> {
+    let mut buf: String = String::new();
+    // stream.read_to_end(&mut buf)?;
+    let mut request = loop {
+        buf_reader.read_line(&mut buf)?;
+        let r = parse_headers(&buf.as_bytes());
+        match r {
+            Err(ReqError::Incomplete) => continue,
+            _ => break r,
+        };
+    }?;
+    // At this point, the buffer should be at start of body
+    // TODO read the body if there is one
+    Ok(request)
+}
 
 /// Parse a buffer into an `http::Request<String>`
 ///
@@ -12,14 +29,15 @@ const MAX_HEADERS: usize = 100;
 ///   - If the parser fails, `ReqError::Parse(httparse::Error)`
 ///   - If the body is not UTF8, `ReqError::Encoding(string::FromUTF8Error)`
 ///   - If the converting to an `http::Request` fails,  `ReqError::Convert(http::Error)`
-pub fn parse_request(buf: &[u8]) -> Result<http::Request<String>, ReqError>  {
+pub fn parse_headers(buf: &[u8]) -> Result<http::Request<String>, ReqError>  {
     let mut headers = [httparse::EMPTY_HEADER; MAX_HEADERS];
     let mut preq = httparse::Request::new(&mut headers);
     
     let result = preq.parse(&buf)?;
     println!("parse result: {:?}", result);
     if let httparse::Status::Complete(body_start) = result {
-        println!("{:#?}", preq);
+        assert!(buf.len() == body_start, 
+                "Header should end with \"\\r\\n\\r\\n\"");
         let request: http::request::Builder = http::Request::builder()
             .method(preq.method.unwrap())
             .uri(preq.path.unwrap());
@@ -37,9 +55,16 @@ pub fn parse_request(buf: &[u8]) -> Result<http::Request<String>, ReqError>  {
 #[derive(Debug)]
 pub enum ReqError {
     Incomplete,
+    IO(std::io::Error),
     Parse(httparse::Error),
     Convert(http::Error),
     Encoding(string::FromUtf8Error),
+}
+
+impl From<std::io::Error> for ReqError {
+    fn from(value: std::io::Error) -> Self {
+        ReqError::IO(value)
+    }
 }
 
 impl From<httparse::Error> for ReqError  {
