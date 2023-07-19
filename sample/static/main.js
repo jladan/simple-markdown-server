64bae8d909c39215ad03ef0f9675f9eae068299c
John Ladan <john@ladan.ca>
2023-07-19 11:33:44 -0400

    update the browser history on partial load

--- sample/static/main.js
+++ sample/static/main.js
@@ -1,7 +1,40 @@
+// Change action of directory browser to insert chunks
+let dirNav = document.querySelector("#left-pane");
+let contentView = document.querySelector("#content-view");
+
+// TODO: This method of updating does not play well with non-html files
+dirNav.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('click detected', event.target)
+    if (event.target.tagName !== 'A') {
+        return;
+    }
+    fetch(event.target, {
+        method: "GET",
+        headers: {
+            "x-partial": "true",
+        },
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        history.pushState({}, 'new page', response.url);
+        return response.text()
+    }).then((body) => {
+        contentView.innerHTML = body;
+    }).catch((error) => {
+        console.log(`Error: ${error.message}`);
+    });
+
+}, {capture: true});
+
+// Collapsible directories
 let navdirbuttons = document.querySelectorAll(".directory-collapse");
 
 for (let btn of navdirbuttons) {
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
         btn.parentElement.classList.toggle("collapsed");
-    });
+    }, {capture: true});
 }
+
